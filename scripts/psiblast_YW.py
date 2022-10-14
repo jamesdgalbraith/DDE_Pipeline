f4e22a976a0f99f1607135c4b14703c2895882ea
jamesdgalbraith <galbraithenator@gmail.com>
2022-10-14 14:19:16 +0100

    Updated psiblast to split fasta, construct initial pssms, run either psiblast or tblastn depending on option

--- scripts/psiblast_YW.py
+++ scripts/psiblast_YW.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from Bio import AlignIO, SeqIO
+from Bio.Seq import Seq
 import Bio.Align
 import os
 from re import sub
@@ -10,6 +11,12 @@ from multiprocessing import Pool
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--in_seq', type=str, required=True,
                     help='Input MSA to be trimmed. Must be fasta with .fasta extension')
+parser.add_argument('-d', '--database', type=str, required = True,
+                    help='Database to be searched')
+parser.add_argument('-o', '--out_dir', type=str, required = True,
+                    help='Directory for output')
+parser.add_argument('-s', '--db_type', type=str, default = 'prot',
+                    help='Sequence type of satabase being searched. Default protein (prot)')
 parser.add_argument('-t', '--threads', type=int, default=1,
                     help='Number of psiblast processes to run at once')
 parser.add_argument('-b', '--blast_threads', type=int, default=1,
@@ -20,50 +27,56 @@ args = parser.parse_args()
 
 # set variables
 subfamily = sub('.fasta', '', sub('.*/', '', args.in_seq))
-out_path = "data/rearranged/"+subfamily
+out_path = args.out_dir+"/"+subfamily
 alignment_in = AlignIO.read(args.in_seq, 'fasta')
+folders_to_make=[out_path, out_path+'/split/', out_path+'/alignments/', out_path+'/pssms/', out_path+'/xml/', out_path+'/fastas/', out_path+'/new_pssms/', out_path+'/tbl/', out_path+'/initial/']
 
 # check files and folders exists
-if(os.path.exists(args.in_seq) == False):
+if os.path.exists(args.in_seq) == False:
   sys.exit('File not found')
 
-if os.path.exists(out_path) is False:
-  os.makedirs(out_path)
-if os.path.exists(out_path+'/alignments/') is False:
-  os.makedirs(out_path+'/alignments/') 
-if os.path.exists(out_path+'/pssms/') is False:
-  os.makedirs(out_path+'/pssms/') 
-if os.path.exists(out_path+'/xml/') is False:
-  os.makedirs(out_path+'/xml/') 
-if os.path.exists(out_path+'/fastas/') is False:
-  os.makedirs(out_path+'/fastas/') 
-if os.path.exists(out_path+'/new_pssms/') is False:
-  os.makedirs(out_path+'/new_pssms/') 
+# func to check all out folders exist
+def folder_check(x):
+  if os.path.exists(x) is False:
+    os.makedirs(x)
 
-# func to find paths
-def job_list(x, y):
-  pssm_in_path = x+'/pssms/'+y+".pssm"
-  new_pssm_out_path = x+'/new_pssms/'+y+".pssm"
-  xml_out_path = x+'/xml/'+y+".xml"
-  to_run = ([pssm_in_path, xml_out_path, new_pssm_out_path],)
-  return(to_run)
+# func to run tblastn/psiblast
+def psiblast_p(y):
+  print(y)
+  os.system('psiblast -subject '+out_path+'/split/'+y+'.fasta -in_msa '+args.in_seq+' -out_pssm '+out_path+'/pssms/'+y+'.pssm -out '+out_path+'/initial/'+y+'.xml -outfmt 5')
+  if args.db_type == 'prot':
+    os.system('psiblast -in_pssm '+out_path+'/pssms/'+y+'.pssm -db '+args.database+' -out '+out_path+'/xml/'+y+'.xml -num_threads '+str(args.blast_threads)+' -num_iterations '+str(args.iterations)+' -outfmt 5 -max_target_seqs 999999 -evalue 1e-5 -out_pssm '+out_path+'/new_pssms/'+y+'.pssm')
+  else:
+    os.system('tblastn -in_pssm '+out_path+'/pssms/'+y+'.pssm -db '+args.database+' -out '+out_path+'/xml/'+y+'.xml -num_threads '+str(args.blast_threads)+' -outfmt 5 -max_target_seqs 999999 -evalue 1e-5')
 
-# func to run psiblast
-def psiblast_p(paths):
-  print(sub('.pssm', '', sub('.*/', '', paths[0])))
-  os.system('psiblast -in_pssm '+paths[0]+' -db /home/james/databases/nr/nr -out '+paths[1]+' -num_threads '+str(args.blast_threads)+' -num_iterations '+str(args.iterations)+' -outfmt 5 -max_target_seqs 999999 -evalue 1e-5 -out_pssm '+paths[2])
-
-paths = ()
-for x in range(len(alignment_in)):
-  paths += job_list(out_path, alignment_in[x].id)
-
+# func to parallelise paths
 def pool_handeler():
   p = Pool(args.threads)
   p.map(psiblast_p, paths)
 
+# check folders exist
+for x in folders_to_make:
+  folder_check(x)
+
+# make database
+if args.db_type == 'prot':
+  if os.path.exists(args.database+'.pdb') == False:
+    os.system('makeblastdb -in '+args.database+' -dbtype prot -out '+args.database)
+else:
+  if os.path.exists(args.database+'.ndb') == False:
+    os.system('makeblastdb -in '+args.database+' -dbtype nucl -out '+args.database)
+
+# split fasta file and make pssms
+with open(args.in_seq, 'r') as handle:
+    for record in SeqIO.parse(handle, "fasta"):
+      record.seq=Seq(sub('-', '', str(record.seq)))
+      file_name = (out_path+'/split/'+record.name.split(sep="#")[0]+".fasta")
+      SeqIO.write(record, file_name, "fasta-2line")
+
+# make list of alignment sequences
+for x in range(len(alignment_in)):
+  if x == 0:
+    paths = []
+  paths.append(alignment_in[x].id)
+
 pool_handeler()
-
-# for x in range(len(paths)):
-#   psiblast_p(paths[x])
-
-
