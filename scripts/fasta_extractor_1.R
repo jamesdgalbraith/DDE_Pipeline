8e174ceaeb1bb00ac0c8387380373e59a6e833bf
jamesdgalbraith <galbraithenator@gmail.com>
2022-10-17 17:19:33 +0100

    fasta extractor to extract fasta from blast out table

--- scripts/fasta_extractor_1.R
+++ scripts/fasta_extractor_1.R
@@ -1,21 +1,18 @@
 library(tidyverse)
-library(Biostrings)
 
-repeat_family <- "Novosib"
-first_d = 17
-last_e = 35
-query_names <- list.files(paste0("data/rearranged/", repeat_family, "_YW/tbl/"))
+repeat_family <- "CMC"
+query_names <- list.files(paste0("data/nr/", repeat_family, "_YW/tbl/"))
 
 for( i in seq_along(query_names)){
   if(i == 1){
-    table_1 <- read_tsv(paste0("data/rearranged/", repeat_family, "_YW/tbl/", query_names[i]),
+    table_1 <- read_tsv(paste0("data/nr/", repeat_family, "_YW/tbl/", query_names[i]),
                         col_names = c("qseqid", "sseqid", "pident", "length", "mismatch",
                                       "gapopen", "qstart", "qend", "sstart", "send",
                                       "evalue", "bitscore", "species", "iteration",
                                       "qlen", "stitle", "Hit_seq"))
   } else{
     table_1 <- rbind(table_1,
-                     read_tsv(paste0("data/rearranged/", repeat_family, "_YW/tbl/", query_names[i]),
+                     read_tsv(paste0("data/nr/", repeat_family, "_YW/tbl/", query_names[i]),
                               col_names = c("qseqid", "sseqid", "pident", "length", "mismatch",
                                             "gapopen", "qstart", "qend", "sstart", "send",
                                             "evalue", "bitscore", "species", "iteration",
@@ -23,15 +20,52 @@ for( i in seq_along(query_names)){
   } 
 }
 
-
-single_species_tbl <- table_1 %>%
-  filter(qstart <= first_d, qend >= qlen - last_e) %>%
+# select best hits
+best_hits <- table_1 %>%
+  filter(length >= 0.9*qlen) %>%
   group_by(sseqid) %>%
   arrange(sseqid, -length) %>%
   dplyr::slice(1) %>%
   ungroup()
 
-single_species_seq <- AAStringSet(single_species_tbl$Hit_seq)
-names(single_species_seq) <- paste0(single_species_tbl$sseqid, ":", single_species_tbl$sstart, "-", single_species_tbl$send, "#", gsub(" ", "_", single_species_tbl$species))
-if(!dir.exists(paste0("data/rearranged/", repeat_family, "_YW/extracted_fastas/"))){dir.create(paste0("data/rearranged/", repeat_family, "_YW/extracted_fastas/"))}
-writeXStringSet(single_species_seq, paste0("data/rearranged/", repeat_family, "_YW/extracted_fastas/", repeat_family, "_YW.fasta"))
+# get sequences
+single_species_seq <- AAStringSet(best_hits$Hit_seq)
+names(single_species_seq) <- paste0(best_hits$sseqid, ":", best_hits$sstart, "-", best_hits$send, "#", gsub(" ", "_", best_hits$species))
+if(!dir.exists(paste0("data/nr/", repeat_family, "_YW/extracted_fastas/"))){dir.create(paste0("data/nr/", repeat_family, "_YW/extracted_fastas/"))}
+writeXStringSet(single_species_seq, paste0("data/nr/", repeat_family, "_YW/extracted_fastas/", repeat_family, "_YW.fasta"))
+
+# plotting
+new_hits<-table_1 %>%
+  filter(length >= 0.9*qlen) %>%
+  group_by(sseqid) %>%
+  arrange(sseqid, iteration) %>%
+  dplyr::slice(1) %>%
+  ungroup() 
+
+new_hits<-as_tibble(as.data.frame(table(new_hits$iteration))) %>% mutate(Var1 = as.integer(Var1))
+
+new_hits$cum_sum <- cumsum(new_hits$Freq)
+
+ggplot(new_hits, aes(x = Var1, y = cum_sum)) + geom_line() +
+  scale_x_continuous(limits = c(1,6), expand = c(0,0),
+                     name = "Iteration") +
+  scale_y_continuous(limits = c(0,max(new_hits$cum_sum)*1.05), expand = c(0,0),
+                     name = ("Cumulative elements found")) +
+  theme_bw()
+
+# only use 1
+for_heatmap_a <- table_1 %>%
+  filter(length >= 0.9*qlen) %>%
+  dplyr::select(qseqid, sseqid) %>%
+  base::unique()
+
+for_heatmap_b <- for_heatmap_a %>% dplyr::rename(qseqid_b = qseqid)
+
+for_heatmap_joined <- full_join(for_heatmap_a, for_heatmap_b) %>%
+  dplyr::select(-sseqid)
+
+for_heatmap <- as_tibble(as.data.frame(table(for_heatmap_joined$qseqid, for_heatmap_joined$qseqid_b))) %>%
+  mutate(Var1 = as.character(Var1), Var2 = as.character(Var2))
+
+ggplot(for_heatmap, aes(x = Var1, y = Var2, fill = Freq)) + geom_tile() + theme_bw() +
+  scale_fill_distiller(palette = "YlGnBu")
---
+++ scripts/repbase_fasta_extractor.R
@@ -0,0 +1,58 @@
+library(tidyverse)
+library(Biostrings)
+
+repeat_family <- "Ginger"
+query_names <- list.files(paste0("data/Repbase/", repeat_family, "_YW/tbl/"))
+
+for( i in seq_along(query_names)){
+  if(i == 1){
+    table_1 <- read_tsv(paste0("data/Repbase/", repeat_family, "_YW/tbl/", query_names[i]),
+                        col_names = c("qseqid", "sseqid", "pident", "length", "mismatch",
+                                      "gapopen", "qstart", "qend", "sstart", "send",
+                                      "evalue", "bitscore", "species", "iteration",
+                                      "qlen", "Hit_seq", "Hit_class"))
+  } else{
+    table_1 <- rbind(table_1,
+                     read_tsv(paste0("data/Repbase/", repeat_family, "_YW/tbl/", query_names[i]),
+                              col_names = c("qseqid", "sseqid", "pident", "length", "mismatch",
+                                            "gapopen", "qstart", "qend", "sstart", "send",
+                                            "evalue", "bitscore", "species", "iteration",
+                                            "qlen", "Hit_seq", "Hit_class")))
+  } 
+}
+
+table(best_hits$Hit_class)
+best_hits <- table_1 %>%
+  filter(length >= 0.9*qlen) %>%
+  group_by(sseqid) %>%
+  arrange(-bitscore) %>%
+  dplyr::slice(1) %>%
+  ungroup()
+
+bleeding <- best_hits %>%
+  filter(!Hit_class %in% c(repeat_family, "Ginger1", "DNA", "Ginger2/TDD", "MuDR", "EnSpm/CACTA", "Transib", "Kolobok",
+                           "Harbinger", "ISL2EU", "Mariner/Tc1", "hAT", "Sola1", "Sola2", "Sola3"))
+
+best_hits <- best_hits  %>%
+  filter(Hit_class %in% c("Ginger1", "Ginger2/TDD")) 
+
+
+# single_species_seq <- AAStringSet(best_hits$Hit_seq)
+# names(single_species_seq) <- paste0(best_hits$sseqid, ":", best_hits$sstart, "-", best_hits$send, "#", gsub(" ", "_", best_hits$species))
+# if(!dir.exists(paste0("data/Repbase/", repeat_family, "_YW/extracted_fastas/"))){dir.create(paste0("data/Repbase/", repeat_family, "_YW/extracted_fastas/"))}
+# writeXStringSet(single_species_seq, paste0("data/Repbase/", repeat_family, "_YW/extracted_fastas/", repeat_family, "_YW.fasta"))
+
+if(nrow(bleeding) > 0){
+  
+  bleeding_classes <- as_tibble(as.data.frame(table(bleeding$Hit_class))) %>% dplyr::rename(Hit_class = Var1) %>% dplyr::mutate(Hit_class = as.character(Hit_class))
+  bleeding_classes
+  for(i in seq_along(bleeding_classes$Hit_class)){
+    class_in_q <- bleeding[toupper(bleeding$Hit_class) == toupper(bleeding_classes$Hit_class[i]),]
+    class_in_q
+    class_in_q_seq <- AAStringSet(class_in_q$Hit_seq)
+    names(class_in_q_seq) <- paste0(class_in_q$sseqid, ":", class_in_q$sstart, "-", class_in_q$send, "#", gsub(" ", "_", class_in_q$species))
+    if(!dir.exists(paste0("data/Repbase/", repeat_family, "_YW/extracted_fastas/others/"))){dir.create(paste0("data/Repbase/", repeat_family, "_YW/extracted_fastas/others/"))}
+    writeXStringSet(class_in_q_seq, paste0("data/Repbase/", repeat_family, "_YW/extracted_fastas/others/", bleeding_classes$Hit_class[i], "_", repeat_family, "_YW.fasta"))
+  }
+
+}
