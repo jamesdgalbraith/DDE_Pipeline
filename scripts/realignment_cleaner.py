34297838032dcc36072cdea9782e8388aad28d48
jamesdgalbraith <galbraithenator@gmail.com>
2022-10-19 12:27:00 +0100

    Made all inputs optional in realignment cleaner

--- scripts/realignment_cleaner.py
+++ scripts/realignment_cleaner.py
@@ -10,15 +10,15 @@ import argparse
 from multiprocessing import Pool
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--family', type=str, required=True,
-                    help='Name of family to be fixed')
+parser.add_argument('-a', '--alignment', type=str, required=True,
+                    help='Existing alignment')
+parser.add_argument('-u', '--unaligned', type=str, required=True,
+                    help='Fasta with sequences to be removed')
+parser.add_argument('-o', '--out', type=str, required=True,
+                    help='Out file')
 args = parser.parse_args()
 
-rb_seq = 'unaligned_seq/'+args.family+'_Repbase.fasta'
-align_seq = 'unaligned_seq/'+args.family+'_YW.fasta'
-out_seq = 'unaligned_seq/'+args.family+'_cleaned_Repbase.fasta'
-
-alignment_in = AlignIO.read(align_seq, 'fasta')
+alignment_in = AlignIO.read(args.alignment, 'fasta')
 
 for x in range(len(alignment_in)):
   if x == 0:
@@ -26,8 +26,8 @@ for x in range(len(alignment_in)):
   else:
     id_list.append(alignment_in[x].id)
 
-with open(rb_seq, 'r') as handle:
-  with open(out_seq, 'w') as briefcase:
+with open(args.unaligned, 'r') as handle:
+  with open(args.out, 'w') as briefcase:
     for record in SeqIO.parse(handle, "fasta"):
         if record.name not in id_list:
           SeqIO.write(record, briefcase, "fasta-2line")
