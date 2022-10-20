8aa1a899f9f2ebee3f0f35dc123317d58f9322c1
jamesdgalbraith <galbraithenator@gmail.com>
2022-10-20 17:32:32 +0100

    realignment_cleaner now ignores case and allows coordinates+species

--- scripts/realignment_cleaner.py
+++ scripts/realignment_cleaner.py
@@ -22,13 +22,13 @@ alignment_in = AlignIO.read(args.alignment, 'fasta')
 
 for x in range(len(alignment_in)):
   if x == 0:
-    id_list = [alignment_in[x].id]
+    id_list = [alignment_in[x].id.upper()]
   else:
-    id_list.append(alignment_in[x].id)
+    id_list.append(alignment_in[x].id.upper())
 
 with open(args.unaligned, 'r') as handle:
   with open(args.out, 'w') as briefcase:
     for record in SeqIO.parse(handle, "fasta"):
-        if record.name not in id_list:
+        if re.sub(":.*", "", record.name.upper()) not in id_list:
           SeqIO.write(record, briefcase, "fasta-2line")
 
