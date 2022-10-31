1481e7aedee5092d1fea81e9afb1967bc4920f70
jamesdgalbraith <galbraithenator@gmail.com>
2022-10-31 16:04:18 +0000

    Made fasat to nexus convereter

---
+++ scripts/fasta_to_nexus.py
@@ -0,0 +1,12 @@
+from Bio import SeqIO
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--in', type=str, required=True,
+                    help='Input FASTA MSA')
+parser.add_argument('-o', '--out', type=str, required=True,
+                    help='Output NEXUS')
+args = parser.parse_args()
+
+records = SeqIO.parse(args.in, "fasta")
+count = SeqIO.write(args.out, "nexus")
