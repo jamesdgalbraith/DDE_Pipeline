8de4906c5e5630f104abfb101fcebaf46deff79b
jamesdgalbraith <galbraithenator@gmail.com>
2022-10-31 16:16:39 +0000

    Hopefullt last revisions to convertor

--- scripts/fasta_to_nexus.py
+++ scripts/fasta_to_nexus.py
@@ -4,12 +4,12 @@ from Bio import SeqIO
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--in', type=str, required=True,
+parser.add_argument('-i', '--in_seq', type=str, required=True,
                     help='Input FASTA MSA')
-parser.add_argument('-o', '--out', type=str, required=True,
+parser.add_argument('-o', '--out_seq', type=str, required=True,
                     help='Output NEXUS')
 args = parser.parse_args()
 
-records = SeqIO.parse(args.in, "fasta")
-count = SeqIO.write(records, args.out, "nexus")
+records = SeqIO.parse(args.in_seq, "fasta")
+count = SeqIO.write(records, args.out_seq, "nexus")
 print("Converted %i records" % count)
